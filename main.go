a1088979c9a1186e7543d79a6d61b021d45d07e4
software-engr-full-stack <software.engr.full.stack@gmail.com>
2023-03-22 18:51:04 -0700

    Clean up

--- main.go
+++ main.go
@@ -11,27 +11,12 @@ import (
     "github.com/jmoiron/sqlx"
     _ "github.com/lib/pq"
 
-    // "virtual-assistant/services/user/pkg/models"
     "virtual-assistant/services/user/pkg/models/postgres"
 )
 
-// var db *sqlx.DB
-
-// type application struct {
-//     errorLog *log.Logger
-//     infoLog  *log.Logger
-//     // users         interface {
-//     //     Insert(string, string, string) error
-//     //     Authenticate(string, string) (int, error)
-//     //     Get(int) (*models.User, error)
-//     // }
-// }
-
 func main() {
     errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
-    // infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 
-    // url := os.Getenv("VA_USERS_URL")
     url := "postgres:///va_users?host=/var/run/postgresql&sslmode=disable"
 
     db, err := sqlx.Open("postgres", url)
@@ -46,9 +31,6 @@ func main() {
 
     svc := userService{
         db: &postgres.UserModel{DB: db},
-        // DB: db,
-        // errorLog: errorLog,
-        // infoLog:  infoLog,
     }
 
     createHandler := httptransport.NewServer(
